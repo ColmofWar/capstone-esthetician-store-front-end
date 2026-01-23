0efc57a6295a2d687974feb91bf6f9023e97c494
ColmofWar <joshuaallendimarco@gmail.com>
2026-01-22 20:19:38 -0500

    moved back end files to seperate repository

---
+++ src/components/Product.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Product() {
+    return (
+        <div>
+            <h1>Here is the {params.name} product page</h1>
+            
+        </div>
+    )
+}
+
+export default Profile;
